4089374833ffea7f51c192b59e1aaf8f337f2662
Joseph Simpson <joe@radicalappdev.com>
2023-03-25 08:26:03 -0400

    Adding babylon js

--- pages/labs/index.vue
+++ pages/labs/index.vue
@@ -3,3 +3,4 @@
     <br>
     <nuxt-link to="/labs/lab001" class="mx-2">Lab 001</nuxt-link>
 </template>
+
--- pages/labs/lab001.vue
+++ pages/labs/lab001.vue
@@ -1,3 +1,64 @@
 <template>
-    <div>Lab 001</div>
+    <div><h3>Lab 001</h3>
+    <canvas style="overflow: hidden" id="bjsCanvas" ref="bjsCanvas" /></div>
 </template>
+
+<script setup>
+import * as BABYLON from "babylonjs";
+
+const bjsCanvas = ref(null);
+
+const createScene = async (canvas) => {
+  // Create and customize the scene
+  const engine = new BABYLON.Engine(canvas);
+  const scene = new BABYLON.Scene(engine);
+
+    // Create a camera
+    const camera = new BABYLON.ArcRotateCamera(
+      "camera",
+      Math.PI / 2,
+      Math.PI / 2,
+      2,
+      BABYLON.Vector3.Zero(),
+      scene
+    );
+
+    // Create a light
+    const light = new BABYLON.HemisphericLight(
+      "light",
+      new BABYLON.Vector3(1, 1, 0),
+      scene
+    );
+
+    // Create a sphere
+    const sphere = BABYLON.MeshBuilder.CreateSphere(
+      "sphere",
+      { diameter: 1 },
+      scene
+    );
+
+    // Create a ground
+    const ground = BABYLON.MeshBuilder.CreateGround(
+      "ground",
+      { width: 6, height: 6 },
+      scene
+    );
+
+
+  engine.runRenderLoop(() => {
+    scene.render();
+  });
+  window.addEventListener("resize", function () {
+    engine.resize();
+  });
+
+
+};
+
+onMounted(() => {
+  if (bjsCanvas.value) {
+    createScene(bjsCanvas.value);
+  }
+});
+
+</script>
