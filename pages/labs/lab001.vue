76cca4a8a3b9154f6d594cd21945a1f96b8a7731
Joseph Simpson <joe@radicalappdev.com>
2023-03-25 17:59:10 -0400

    recreation of lab 001

--- pages/labs/lab001.vue
+++ pages/labs/lab001.vue
@@ -2,7 +2,7 @@
   <div>
     <h3 class="text-lg font-bold">Lab 001</h3>
     <p>
-      This is a temporary lab for testing the use of the new labtools.
+      This is a recreation of the original Canvatorium Lab 001. I picked out some colors to use throughout the labs in the future. I created a box for each color and used the standard BabylonJS material.
     </p>
     <div>
       <canvas id="bjsCanvas" ref="bjsCanvas" />
@@ -12,16 +12,54 @@
 
 <script setup>
 import { ref } from "vue";
+import { MeshBuilder, StandardMaterial, Color3 } from "babylonjs";
 
 const bjsCanvas = ref(null);
 
 // Add lab-specific content here using the provided 'scene' instance
 const createLabContent = (scene) => {
 
-  // Add a box
-    const sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {});
-    sphere.position.y = 0.5;
+  // Make some boxes to test out the colors in VR
+  const group = new BABYLON.Mesh("color-group");
+  group.position = new BABYLON.Vector3(-3.5, 0.5, 0);
 
+  // Add color boxes
+  makeBox("purple", group, scene).position = new BABYLON.Vector3(0, 0, 0);
+  makeBox("blue", group, scene).position = new BABYLON.Vector3(1, 0, 0);
+  makeBox("teal", group, scene).position = new BABYLON.Vector3(2, 0, 0);
+  makeBox("cyan", group, scene).position = new BABYLON.Vector3(3, 0, 0);
+
+  makeBox("green", group, scene).position = new BABYLON.Vector3(4, 0, 0);
+  makeBox("yellow", group, scene).position = new BABYLON.Vector3(5, 0, 0);
+  makeBox("orange", group, scene).position = new BABYLON.Vector3(6, 0, 0);
+  makeBox("red", group, scene).position = new BABYLON.Vector3(7, 0, 0);
+
+  // Add slate boxes
+  makeBox("slate1", group, scene).position = new BABYLON.Vector3(0, 1, 0);
+  makeBox("slate2", group, scene).position = new BABYLON.Vector3(1, 1, 0);
+  makeBox("slate3", group, scene).position = new BABYLON.Vector3(2, 1, 0);
+  makeBox("slate4", group, scene).position = new BABYLON.Vector3(3, 1, 0);
+  makeBox("slate5", group, scene).position = new BABYLON.Vector3(4, 1, 0);
+  makeBox("slate6", group, scene).position = new BABYLON.Vector3(5, 1, 0);
+  makeBox("slate7", group, scene).position = new BABYLON.Vector3(6, 1, 0);
+  makeBox("slate8", group, scene).position = new BABYLON.Vector3(7, 1, 0);
+
+};
+
+const makeBox = (colorName, parent, scene) => {
+  // Create a colored box from using a string to get the color from the Brand object
+  const mat = new StandardMaterial(`${colorName}-material`, scene);
+  mat.diffuseColor = new Color3.FromHexString(labColors[colorName]);
+  mat.specularColor = new Color3(0.1, 0.1, 0.1);
+  const mesh = MeshBuilder.CreateBox(
+    `${colorName}-box`,
+    { size: 1 },
+    scene
+  );
+  mesh.material = mat;
+  mesh.parent = parent;
+
+  return mesh;
 };
 
 // Without scene options (see lab000.vue for an example)
