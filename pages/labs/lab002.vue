078ed3ffcbd595c3d613a02bf60aef3382b678c9
Joseph Simpson <joe@radicalappdev.com>
2023-03-29 15:23:04 -0400

    Basic ref version of lab 002

--- pages/labs/lab002.vue
+++ pages/labs/lab002.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { MeshBuilder } from "babylonjs";
+import { AbstractMesh, MeshBuilder, Vector3 } from "babylonjs";
+import { AdvancedDynamicTexture, GUI3DManager, Button3D, TextBlock } from "babylonjs-gui";
 
 definePageMeta({
     featured: false,
@@ -7,13 +8,66 @@ definePageMeta({
     description: 'Lab 002 – watch and watchEffect'
 })
 
+const sample = ref("default");
+const count = ref(0);
+let reactiveScene;
+
 const createLabContent = async (scene)  => {
     
     
-    MeshBuilder.CreateBox("box", { size: 1 }, scene).position.y = 0.5;
+    reactiveScene = scene;
+    let anchor = new AbstractMesh("anchor", scene);
+    let manager = new GUI3DManager(scene);
+
+    makeCard(scene);
+    makeButton(scene, manager, anchor);
     
     
 };
+
+const makeCard =(scene) => {
+  
+  var plane = MeshBuilder.CreatePlane("plane", { size: 0.5}, scene);
+  plane.position.y = 2;
+
+  var advancedTexture = AdvancedDynamicTexture.CreateForMesh(plane);
+  advancedTexture.name = "card-texture";
+  advancedTexture.background = labColors.slate5;
+
+  var cardText = new TextBlock("card-text");
+  cardText.text = "Watch";
+  cardText.color = "white";
+  cardText.fontSize = 64;
+
+  advancedTexture.addControl(cardText);
+  plane.scaling = new Vector3(5, 5, 5);
+};
+
+const makeButton = (scene, manager, anchor) => {
+  // Let's add a button
+  var button = new Button3D("reset");
+  manager.addControl(button);
+  button.linkToTransformNode(anchor);
+  button.position.y = 1;
+
+  var text1 = new TextBlock();
+  text1.text = "Change Values";
+  text1.color = "white";
+  text1.fontSize = 24;
+  button.content = text1;
+
+  button.onPointerUpObservable.add(() => {
+    count.value++;
+    sample.value = "Modified";
+  });
+};
+
+// Watch with a single value
+watch(count, (newValue, oldValue) => {
+  const texture = reactiveScene.getTextureByName("card-texture");
+  texture.getControlByName("card-text").text =
+    "From " + oldValue + " to " + newValue;
+});
 
 const bjsCanvas = ref(null);
 useCanvatoriumScene(bjsCanvas, createLabContent);
