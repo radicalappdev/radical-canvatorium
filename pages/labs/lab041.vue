d3b9caee503ad18f56abf0a6804da0b3a0599f98
Joseph Simpson <joe@radicalappdev.com>
2023-03-30 07:11:48 -0400

    Adding some notes

--- pages/labs/lab041.vue
+++ pages/labs/lab041.vue
@@ -4,7 +4,7 @@ import { MeshBuilder, Tools } from "babylonjs";
 definePageMeta({
     featured: true,
     title: 'Lab 041 - Screenshots with Babylon.js',
-    description: 'Taking screenshots with Babylon.js.'
+    description: 'Press the S key to take a screenshot with Babylon.js'
 })
 
 const createLabContent = async (scene)  => {
@@ -13,7 +13,8 @@ const createLabContent = async (scene)  => {
     const camera = scene.activeCamera;
     MeshBuilder.CreateBox("box", { size: 1 }, scene).position.y = 0.5;  
     
-    
+    // For the purposes of this lab, we'll use the keyboard observable to take a screenshot
+    // When I add this to the useCanvatoriumScene composable, I'll call it from a button on the GUI
     // Take a screenshot of the scene when I press the 's' key 
     scene.onKeyboardObservable.add((kbInfo) => {
         if (kbInfo.type === BABYLON.KeyboardEventTypes.KEYDOWN && kbInfo.event.key === "s") {
@@ -25,8 +26,6 @@ const createLabContent = async (scene)  => {
             console.log("screenshot created", screenshot);
         }
     });
-    
-    
     
     
     
