a7a22e43f0e6537cf9f757333616c25c47e3c167
Joseph Simpson <joe@radicalappdev.com>
2023-03-31 07:17:19 -0400

    Adding a new tab bar for  lab and lab notes

--- app.vue
+++ app.vue
@@ -8,11 +8,11 @@
 </template>
 
 <style>
-#bjsCanvas {
+/* #bjsCanvas {
   position: absolute;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-}
+} */
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="flex flex-col min-h-screen">
-    <header class="fixed top-0 w-full h-12 bg-slate-800 text-white flex items-center justify-between px-4">
-      <h1 class="text-lg font-bold">Canvatorium (revamped)</h1>
-      <div class="flex">
-        <nuxt-link to="/" class="mx-2">Featured Labs</nuxt-link>
-        <nuxt-link to="/labs" class="mx-2">All Labs</nuxt-link>
+    <header class="fixed top-0 w-full h-24 bg-slate-800 text-white px-4">
+      <div class="flex flex-col">
+        
+        <div class="flex flex-row items-center justify-between my-2">
+          <h1 class="text-lg font-bold">Canvatorium <span class="hidden sm:inline">(revamped)</span></h1>
+          
+          <div class="flex">
+            <nuxt-link to="/" class="mx-2">Featured</nuxt-link>
+            <nuxt-link to="/labs" class="mx-2">All Labs</nuxt-link>
+          </div>
+        </div>
+        
+        <div class="flex flex-row w-full mt-1">
+          <button @click="showLab" :class="{'border border-slate-500 rounded-l-md bg-slate-900 text-white font-semibold focus:outline-none': activeTab === 'lab', 'bg-slate-700 rounded-l-md': activeTab !== 'lab'}" class="flex-1 py-1 focus:outline-none">Lab Scene</button>
+          <button @click="showLabNotes" :class="{'border border-slate-500 rounded-r-md bg-slate-900 text-white font-semibold focus:outline-none': activeTab === 'lab-notes', 'bg-slate-700 rounded-r-md': activeTab !== 'lab-notes'}" class="flex-1 py-1 focus:outline-none">Lab Notes</button>
+        </div>
+        
       </div>
     </header>
-    <main class="flex-1">
-        <div class="mx-4 py-4">
-            <slot />
+    
+    
+    <main>
+      <div class="lab-container" :class="labClasses()">
+        <slot />
+      </div>
+
+      <div class="lab-notes" :class="labNotesClasses()">
+        <div class="container mx-auto my-4 p-4 bg-white max-w-screen-md h-[calc(100%_-_2rem)] overflow-y-auto">
+          <h3 class="text-xl font-semibold">{{ $route.meta.title }}</h3>
+          <p>{{ $route.meta.description }}</p>
         </div>
-        </main>
-    <footer class="fixed bottom-0 w-full h-8 bg-slate-800 text-white flex items-center justify-center">
+        
+      </div>
+      
+    </main>
+    <footer class="fixed bottom-0 w-full h-8 bg-slate-800 text-sm text-white flex items-center justify-center">
       <p>Canvatorium © Radical Application Development {{ year }}</p>
     </footer>
   </div>
@@ -20,19 +43,77 @@
 
 <script setup>
 const year = new Date().getFullYear()
-</script>
 
-<style>
-/* Add styles to the main element to fill the viewport */
-main {
-  top: 3rem;
-  bottom: 2rem;
-  height: calc(100vh - 5rem);
-  left: 0;
-  right: 0;
-  position: fixed;   
-  overflow: scroll;
+const route = useRoute()
+
+// Create a new reactive variable to store the active tab
+const activeTab = ref('lab')
+
+// Update the active tab when a button is clicked
+const showLab = () => {
+  activeTab.value = 'lab'
 }
 
-</style>
+const showLabNotes = () => {
+  activeTab.value = 'lab-notes'
+}
 
+// Add computed classes to show or hide the lab and lab-notes components based on the active tab
+const labClasses = () => ({
+  'hidden': activeTab.value !== 'lab'
+})
+
+const labNotesClasses = () => ({
+  'hidden': activeTab.value !== 'lab-notes'
+})
+
+
+
+// const showLab = () => {
+  //   const labContainer = document.querySelector('.lab-container')
+  //   const labNotes = document.querySelector('.lab-notes')
+  
+  //   labContainer.classList.remove('hidden')
+  //   labNotes.classList.add('hidden')
+  // }
+  
+  // const showLabNotes = () => {
+    //   const labContainer = document.querySelector('.lab-container')
+    //   const labNotes = document.querySelector('.lab-notes')
+    
+    //   labContainer.classList.add('hidden')
+    //   labNotes.classList.remove('hidden')
+    // }
+  </script>
+  
+  <style>
+  /* Add styles to the main element to fill the viewport */
+  .lab-container {
+    top: 6rem;
+    bottom: 2rem;
+    height: calc(100vh - 8rem);
+    left: 0;
+    right: 0;
+    position: fixed;   
+    overflow: scroll;
+  }
+  
+  .lab-notes {
+    top: 6rem;
+    bottom: 2rem;
+    height: calc(100vh - 8rem);
+    left: 0;
+    right: 0;
+    position: fixed;   
+    overflow: scroll;
+  }
+  
+  /* Used on the canvas elements in the pages rendered by the slot */
+  #bjsCanvas {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+</style>
