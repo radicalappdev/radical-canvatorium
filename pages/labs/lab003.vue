3bcbfd38a0c0da9b6baaf8ce8c769695e54271b7
Joseph Simpson <joe@radicalappdev.com>
2023-04-04 08:39:14 -0400

    Move the user to the landing platform on XR enter

--- pages/labs/lab003.vue
+++ pages/labs/lab003.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { MeshBuilder } from "babylonjs";
+import { MeshBuilder, Vector3 } from "babylonjs";
 
 definePageMeta({
   featured: true,
@@ -10,12 +10,23 @@ definePageMeta({
 const createLabContent = async (scene) => {
   MeshBuilder.CreateBox("box", { size: 1 }, scene).position.y = 0.5;
 
+  // Create a cylinder
+  const landing = MeshBuilder.CreateCylinder("cylinder", { diameter: 1, height: 0.2 }, scene);
+  landing.position = new Vector3(2, 0, 2);
+
   // get mesh by name 'ground' to use for teleportation - this is created by the labCreateRoom function in useCanvatoriumScene
   const ground = scene.getMeshByName("ground");
   console.log("ground", ground);
 
   const xr = await scene.createDefaultXRExperienceAsync({
     floorMeshes: ground
+  });
+
+  // Move the player when thet enter immersive mode
+  xr.baseExperience.onInitialXRPoseSetObservable.add((xrCamera) => {
+    console.log("Entering Immersive Mode with camera", xrCamera);
+    xrCamera.position.z = landing.position.z;
+    xrCamera.position.x = landing.position.x;
   });
 
   console.log("xr player created", xr);
