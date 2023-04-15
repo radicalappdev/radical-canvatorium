04a016842507d8043ddfc0afe1581b638343c6a5
Joseph Simpson <joe@radicalappdev.com>
2023-04-15 17:05:33 -0400

    Moving to a new local cert process
    ChatGPT came up with most of this

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,18 @@
 import path from "path";
 import fs from "fs";
+const options = require("./generate-cert");
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss"],
   server: {
     https: {
-      key: fs.readFileSync(path.resolve(__dirname, "key.pem")),
-      cert: fs.readFileSync(path.resolve(__dirname, "cert.pem"))
+      key: options.key,
+      cert: options.cert
     }
+    // https: {
+    //   key: fs.readFileSync(path.resolve(__dirname, "key.pem")),
+    //   cert: fs.readFileSync(path.resolve(__dirname, "cert.pem"))
+    // }
   }
 });
