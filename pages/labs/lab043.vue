29b213087332f13166d0a7d206432ca058e966e0
Joseph Simpson <joe@radicalappdev.com>
2023-06-16 14:07:39 -0400

    Adding lots of cards

--- pages/labs/lab043.vue
+++ pages/labs/lab043.vue
@@ -1,5 +1,6 @@
 <script setup>
   import { MeshBuilder } from "babylonjs";
+  import { AdvancedDynamicTexture, Button, TextBlock } from "babylonjs-gui";
 
   definePageMeta({
     featured: false,
@@ -8,7 +9,35 @@
   });
 
   const createLabContent = async (scene) => {
-    MeshBuilder.CreateBox("box", { size: 1 }, scene).position.y = 0.5;
+    const sparticus = generateCard(scene);
+    sparticus.position.x = Math.random() * 10 - 5;
+    sparticus.position.z = Math.random() * 10 - 5;
+    sparticus.position.y = Math.random() + 1.5;
+
+    for (let i = 0; i < 99; i++) {
+      const card = generateCard(scene);
+      card.position.x = Math.random() * 10 - 5;
+      card.position.z = Math.random() * 10 - 5;
+      card.position.y = Math.random() + 1.5;
+    }
+  };
+
+  const generateCard = (scene) => {
+    const plane = MeshBuilder.CreatePlane("plane", { width: 0.5, height: 0.5 }, scene);
+
+    const advancedTexture = AdvancedDynamicTexture.CreateForMesh(plane);
+    advancedTexture.name = "card-texture";
+    advancedTexture.background = labColors.slate8;
+
+    const titleText = new TextBlock("title-text");
+
+    titleText.text = "I'm Sparticus!";
+    titleText.color = "white";
+    titleText.fontSize = 96;
+
+    advancedTexture.addControl(titleText);
+
+    return plane;
   };
 
   const bjsCanvas = ref(null);
