5cf89acd931e338b86bf066ebecd1d71e6071f81
Joseph Simpson <joe@radicalappdev.com>
2023-07-25 11:28:23 -0400

    Show a main window when selecting an item from the collection

--- pages/labs/lab050.vue
+++ pages/labs/lab050.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import { AbstractMesh, MeshBuilder, TransformNode, Vector3 } from "babylonjs";
+  import { AbstractMesh, MeshBuilder, TransformNode, Vector3, Animation } from "babylonjs";
   import { TextBlock, Image, Control, Rectangle, MeshButton3D, GUI3DManager, PlanePanel } from "babylonjs-gui";
   import computingData from "@/data/computing.json";
 
@@ -11,6 +11,7 @@
 
   const createLabContent = async (scene) => {
     // Data and state at parent scope
+    const showMain = ref(false);
     const activeIndex = ref(0);
     const collectionData = reactive(computingData);
     const activeRecord = computed(() => computingData[activeIndex.value]);
@@ -22,9 +23,35 @@
     // Create a window group object. This is a parent object that will contain the window and toolbar planes, and any other objects we want to add to the window. This can be found in lab-uikit.ts
     const windowGroupMesh = canLabWindowGroup(scene);
 
-    const collection = createCollection(collectionData, scene);
-    collection.parent = windowGroupMesh;
-    collection.position = new Vector3(0, 2.9, 0);
+    const anchor = new TransformNode("anchor");
+    const manager = new GUI3DManager(scene);
+    const plainPanel = new PlanePanel();
+    plainPanel.margin = 0.1;
+    plainPanel.columns = 4;
+    manager.addControl(plainPanel);
+    plainPanel.linkToTransformNode(anchor);
+
+    // create a card for each record and add them to the plainPanel
+    collectionData.forEach((record, index) => {
+      const { smallMesh, smallTexture } = lab050_example_1(record, scene);
+      smallMesh.parent = anchor;
+      smallMesh.position = new Vector3(0, 0, 0);
+      smallMesh.scaling = new Vector3(0.45, 0.45, 0.45);
+
+      const button = new MeshButton3D(smallMesh, "button");
+      button.scaling = new Vector3(0.3, 0.3, 0.3);
+
+      button.onPointerUpObservable.add(() => {
+        console.log("Clicked on button");
+        activeIndex.value = index;
+        showMain.value = true;
+      });
+
+      plainPanel.addControl(button);
+    });
+
+    anchor.parent = windowGroupMesh;
+    anchor.position = new Vector3(0, 2.7, 0);
 
     // Create the main content card. This can be found in lab-examples.js
     const { contentMesh } = exampleContent(activeRecord, scene);
@@ -32,6 +59,21 @@
     contentMesh.position = new Vector3(0, 2.7, 0);
     contentMesh.isPickable = false;
     contentMesh.visibility = 0;
+
+    watch(showMain, (newValue) => {
+      if (newValue) {
+        Animation.CreateAndStartAnimation("open-modal", contentMesh, "visibility", 60, 6, 0, 1, 0);
+        contentMesh.isPickable = true;
+
+        // scale the anchor down to 0
+        Animation.CreateAndStartAnimation("open-modal", anchor, "scaling", 60, 6, anchor.scaling, new Vector3(0, 0, 0), 0);
+      } else {
+        Animation.CreateAndStartAnimation("open-modal", contentMesh, "visibility", 60, 6, 1, 0, 0);
+
+        // scale the anchor up to 1
+        Animation.CreateAndStartAnimation("open-modal", anchor, "scaling", 60, 6, anchor.scaling, new Vector3(1, 1, 1), 0);
+      }
+    });
   };
 
   const lab050_example_1 = (record, scene) => {
@@ -85,35 +127,6 @@
     );
 
     return { smallMesh: detailMesh, smallTexture: detailTexture };
-  };
-
-  const createCollection = (collectionData, scene) => {
-    const anchor = new TransformNode("anchor");
-    const manager = new GUI3DManager(scene);
-    const plainPanel = new PlanePanel();
-    plainPanel.margin = 0.1;
-    plainPanel.columns = 4;
-    manager.addControl(plainPanel);
-    plainPanel.linkToTransformNode(anchor);
-
-    // create a card for each record and add them to the plainPanel
-    collectionData.forEach((record) => {
-      const { smallMesh, smallTexture } = lab050_example_1(record, scene);
-      smallMesh.parent = anchor;
-      smallMesh.position = new Vector3(0, 0, 0);
-      smallMesh.scaling = new Vector3(0.3, 0.3, 0.3);
-
-      const button = new MeshButton3D(smallMesh, "button");
-      button.scaling = new Vector3(0.3, 0.3, 0.3);
-
-      button.onPointerDownObservable.add(() => {
-        console.log("Clicked on button");
-      });
-
-      plainPanel.addControl(button);
-    });
-
-    return anchor;
   };
 
   const bjsCanvas = ref(null);
