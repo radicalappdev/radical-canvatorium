07026098256ed35e9cbec763b997032f44cad828
Joseph Simpson <joe@radicalappdev.com>
2023-07-25 08:14:41 -0400

    createCollection will create cards for each record

--- pages/labs/lab050.vue
+++ pages/labs/lab050.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import { Vector3 } from "babylonjs";
+  import { AbstractMesh, Vector3 } from "babylonjs";
   import { TextBlock, Image, Control, Rectangle } from "babylonjs-gui";
   import computingData from "@/data/computing.json";
 
@@ -22,22 +22,9 @@
     // Create a window group object. This is a parent object that will contain the window and toolbar planes, and any other objects we want to add to the window. This can be found in lab-uikit.ts
     const windowGroupMesh = canLabWindowGroup(scene);
 
-    // Create a function that will loop over the collection data
-
-    const createCollection = (collectionData) => {
-      const startPosition = new Vector3(-2.1, 0, 0);
-
-      collectionData.forEach((record, index) => {
-        const offset = index * 1.4;
-        const offsetPosition = startPosition.add(new Vector3(offset, 0, 0));
-        const { smallMesh, smallTexture } = lab050_example_1(record, index, scene);
-        smallMesh.parent = windowGroupMesh;
-        smallMesh.position = offsetPosition;
-        smallMesh.scaling = new Vector3(0.3, 0.3, 0.3);
-      });
-    };
-
-    createCollection(collectionData);
+    const collection = createCollection(collectionData, scene);
+    collection.parent = windowGroupMesh;
+    collection.position = new Vector3(0, 2.9, 0);
 
     // Create the main content card. This can be found in lab-examples.js
     const { contentMesh } = exampleContent(activeRecord, scene);
@@ -47,10 +34,10 @@
     contentMesh.visibility = 0;
   };
 
-  const lab050_example_1 = (activeRecord, index, scene) => {
+  const lab050_example_1 = (activeRecord, scene) => {
     const { plane: detailMesh, advancedTexture: detailTexture } = canLabCardSimple(4, 4.6, scene);
 
-    const cardTextureName = "content-texture-" + index;
+    const cardTextureName = "content-texture-" + activeRecord.id;
     detailTexture.name = cardTextureName;
 
     const imageContainer = new Rectangle("masker");
@@ -96,6 +83,38 @@
 
     return { smallMesh: detailMesh, smallTexture: detailTexture };
   };
+
+  const createCollection = (collectionData, scene) => {
+    const startPosition = new Vector3(-2.1, 0, 0);
+
+    const collectionContainer = new AbstractMesh("collection-container");
+
+    const chunks = chunk(collectionData, 4);
+    chunks.forEach((chunk, rowIndex) => {
+      const rowContainer = new AbstractMesh(`row-${rowIndex}`);
+      rowContainer.position = new Vector3(0, rowIndex * -1.6, 0);
+      rowContainer.parent = collectionContainer;
+
+      chunk.forEach((record, colIndex) => {
+        const offsetX = colIndex * 1.4;
+        const offsetPosition = startPosition.add(new Vector3(offsetX, 0, 0));
+        const { smallMesh, smallTexture } = lab050_example_1(record, scene);
+        smallMesh.parent = rowContainer;
+        smallMesh.position = offsetPosition;
+        smallMesh.scaling = new Vector3(0.3, 0.3, 0.3);
+      });
+    });
+
+    return collectionContainer;
+  };
+
+  function chunk(array, size) {
+    const result = [];
+    for (let i = 0; i < array.length; i += size) {
+      result.push(array.slice(i, i + size));
+    }
+    return result;
+  }
 
   const bjsCanvas = ref(null);
   useCanvatoriumScene(bjsCanvas, createLabContent);
