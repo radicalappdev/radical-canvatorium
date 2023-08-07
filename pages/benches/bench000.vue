d5d6f36ef900f614f8ed6df2013772be65ceecc3
Joseph Simpson <joe@radicalappdev.com>
2023-08-07 15:29:25 -0400

    Getting close to parsing points

--- pages/benches/bench000.vue
+++ pages/benches/bench000.vue
@@ -1,6 +1,7 @@
 <script setup>
   import { Vector3 } from "babylonjs";
   import { TextBlock } from "babylonjs-gui";
+  import "babylonjs-loaders";
 
   definePageMeta({
     featured: false,
@@ -16,10 +17,84 @@
 
     const ohioSVG = await fetch("/assets/usa-oh.svg").then((res) => res.text());
     // console.log("SVG", ohioSVG);
+    // Create an array to store the objects
+    const pathsArray = [];
 
-    // TODO: Create an array of paths from the SVG data
-    // Convert each path to an object that can be extruded
+    // Parse the SVG data to extract the path elements
+    const parser = new DOMParser();
+    const svgDocument = parser.parseFromString(ohioSVG, "image/svg+xml");
+    const pathElements = svgDocument.querySelectorAll("path");
+
+    // Iterate through each path and convert it to an object
+    pathElements.forEach((pathElement) => {
+      // Get the 'd' attribute of the path, which contains the path data
+      const pathData = pathElement.getAttribute("d");
+      console.log(pathData);
+
+      // Parse the path data to get an array of points
+      const points = parsePathData(pathData);
+
+      // Create an object containing the points array
+      const pathObject = {
+        points
+        // Add any other properties you may want for extrusion, e.g., height, color, etc.
+      };
+
+      // Push the object to the pathsArray
+      pathsArray.push(pathObject);
+    });
+
+    // Now you have an array of objects, each containing an array of points.
+    console.log(pathsArray);
   };
+
+  // Function to parse path data and convert it into an array of points
+  function parsePathData(pathData) {
+    // Regular expression pattern to find all commands and their coordinates
+    const regex = /([mlhvcsqtaz])([^mlhvcsqtaz]*)/gi;
+
+    // Array to store the points
+    const points = [];
+
+    let currentPoint = new Vector3(0, 0, 0); // Starting point
+
+    // Helper function to update the current point based on the command and its parameters
+    function updateCurrentPoint(command, parameters) {
+      const [dx, dy] = parameters.split(",").map(parseFloat);
+      switch (command.toLowerCase()) {
+        case "m": // MoveTo
+          currentPoint.x += dx;
+          currentPoint.y += dy;
+          break;
+        case "l": // LineTo
+          currentPoint.x += dx;
+          currentPoint.y += dy;
+          break;
+        case "h": // Horizontal LineTo
+          currentPoint.x += dx;
+          break;
+        case "v": // Vertical LineTo
+          currentPoint.y += dy;
+          break;
+        case "z": // ClosePath (Z)
+          // For simplicity, we ignore 'z' commands (ClosePath) in this example.
+          // If you want to handle closed paths, you can add the necessary logic here.
+          break;
+        default:
+          break;
+      }
+    }
+
+    let match;
+    while ((match = regex.exec(pathData)) !== null) {
+      const command = match[1];
+      const parameters = match[2].trim();
+      updateCurrentPoint(command, parameters);
+      points.push(new Vector3(currentPoint.x, currentPoint.y, 0)); // We assume z=0 in this case, as it's a 2D SVG.
+    }
+
+    return points;
+  }
 
   // If a lab uses the default options, you can just call useBabylonScene() with the bjsCanvas ref and the createLabContent function.
   // Otherwise, you can pass in an options object with the following properties:
