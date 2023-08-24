11f11c04e0cad05a97aea69501aba224710f039d
Joseph Simpson <joe@radicalappdev.com>
2023-08-24 07:48:01 -0400

    Lab 1000 clean up and comments

--- pages/labs/three/lab1000.vue
+++ pages/labs/three/lab1000.vue
@@ -1,9 +1,9 @@
 <script setup>
   import * as THREE from "three";
   import { VRButton } from "three/examples/jsm/webxr/VRButton.js";
-
+  import { BoxLineGeometry } from "three/examples/jsm/geometries/BoxLineGeometry.js";
   import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-  import { Vector3 } from "babylonjs";
+
   definePageMeta({
     featured: false,
     title: "Lab 1000 – Hello Canvatorium + Three JS",
@@ -25,13 +25,22 @@
 
     // Create a camera
     const camera = new THREE.PerspectiveCamera(35, width / height, 0.1, 100);
-    camera.position.set(0, 5, 10);
+    camera.position.set(0, 1.4, 4);
 
     // add the camera to a group
     const cameraGroup = new THREE.Group();
     cameraGroup.add(camera);
     cameraGroup.position.set(0, 0, 2); // position the camera group will impact the XR camera
     scene.add(cameraGroup);
+
+    // Add orbit controls
+    const controls = new OrbitControls(camera, container.value);
+    controls.target.set(0, 1, 0);
+    controls.maxPolarAngle = Math.PI / 2;
+    controls.minPolarAngle = 0;
+    controls.maxDistance = 10;
+    controls.minDistance = 1;
+    controls.update();
 
     // Add ambient light
     const ambientLight = new THREE.AmbientLight(0xffffff, 1);
@@ -42,41 +51,29 @@
     directionalLight.position.set(10, 10, 10);
     scene.add(directionalLight);
 
-    // Add orbit controls
-    const controls = new OrbitControls(camera, container.value);
-    controls.target.set(0, 0, 0);
-    controls.maxPolarAngle = Math.PI / 2;
-    controls.minPolarAngle = 0;
-    controls.maxDistance = 20;
-    controls.minDistance = 5;
-    controls.update();
-
-    // Add a grid to the scene
-    const gridHelper = new THREE.GridHelper(5, 5);
-    scene.add(gridHelper);
+    const room = new THREE.LineSegments(new BoxLineGeometry(20, 10, 20, 20, 10, 20).translate(0, 5, 0), new THREE.LineBasicMaterial({ color: 0xbcbcbc }));
+    scene.add(room);
 
     // Create a cube
     const geometry = new THREE.BoxGeometry(1, 1, 1);
     const material = new THREE.MeshStandardMaterial({ color: labColors.slate3 });
     const cube = new THREE.Mesh(geometry, material);
-    cube.position.set(0, 1, 0);
+    cube.position.set(0, 0.5, 0);
     scene.add(cube);
 
     // Create the renderer
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(width, height); // TODO: This isn't working!
     renderer.setPixelRatio(window.devicePixelRatio);
-    // Add the XR support
+    // Add the XR support and use setAnimationLoop to render the scene instead of requestAnimationFrame
     renderer.xr.enabled = true;
     renderer.setAnimationLoop(function () {
       renderer.render(scene, camera);
     });
-    document.body.appendChild(VRButton.createButton(renderer));
-
-    // position the XR player / camera 2 meters away from the origin
 
     // Add the automatically created <canvas> element to the page
     container.value.append(renderer.domElement);
+    container.value.append(VRButton.createButton(renderer));
   };
 
   // Create the scene when the component is mounted
