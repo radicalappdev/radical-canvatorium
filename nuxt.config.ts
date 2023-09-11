11c3fd0032ce3e1717d8b0e1257af6b9a61ed358
Joseph Simpson <joe@radicalappdev.com>
2023-09-11 08:50:54 -0400

    Simplify the local cert process and documentation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,3 @@
-import path from "path";
-import fs from "fs";
-const options = require("./generate-cert");
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss"],
@@ -11,18 +7,5 @@ export default defineNuxtConfig({
   ],
   routeRules: {
     "/labs/three/**": { ssr: false }
-  },
-  server: {
-    https: {
-      key: options.key,
-      cert: options.cert
-    },
-    alias: {
-      "@": "./"
-    }
-    // https: {
-    //   key: fs.readFileSync(path.resolve(__dirname, "key.pem")),
-    //   cert: fs.readFileSync(path.resolve(__dirname, "cert.pem"))
-    // }
   }
 });
