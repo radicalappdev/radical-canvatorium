6ffbd7708cc58b34f3d55d70de302fb041e29aad
Joseph Simpson <joe@radicalappdev.com>
2023-09-19 15:14:52 -0400

    ts for 041 and clean up

--- pages/labs/lab041.vue
+++ pages/labs/lab041.vue
@@ -1,38 +1,38 @@
-<script setup>
-import { MeshBuilder, Tools } from "babylonjs";
+<script lang="ts" setup>
+  import { Scene, Camera, MeshBuilder, Tools } from "babylonjs";
 
-definePageMeta({
+  definePageMeta({
     featured: true,
-    title: 'Lab 041 - Screenshots with Babylon.js',
-    description: 'Press the S key to take a screenshot with Babylon.js'
-})
+    title: "Lab 041 - Screenshots with Babylon.js",
+    description: "Press the S key to take a screenshot with Babylon.js"
+  });
 
-const createLabContent = async (scene)  => {
-    
+  const createLabContent = async (scene: Scene) => {
     const engine = scene.getEngine();
     const camera = scene.activeCamera;
-    MeshBuilder.CreateBox("box", { size: 1 }, scene).position.y = 0.5;  
-    
+
+    const box = MeshBuilder.CreateBox("box", { size: 1 }, scene);
+    box.position.y = 0.5;
+
     // For the purposes of this lab, we'll use the keyboard observable to take a screenshot
     // When I add this to the useCanvatoriumScene composable, I'll call it from a button on the GUI
-    // Take a screenshot of the scene when I press the 's' key 
+    // Take a screenshot of the scene when I press the 's' key
     scene.onKeyboardObservable.add((kbInfo) => {
-        if (kbInfo.type === BABYLON.KeyboardEventTypes.KEYDOWN && kbInfo.event.key === "s") {
-            const size = {
-                width: engine.getRenderWidth(),
-                height: engine.getRenderHeight()
-            }
-            const screenshot = Tools.CreateScreenshot(engine, camera, size);
-            console.log("screenshot created", screenshot);
-        }
+      if (kbInfo.type === BABYLON.KeyboardEventTypes.KEYDOWN && kbInfo.event.key === "s") {
+        const size = {
+          width: engine.getRenderWidth(),
+          height: engine.getRenderHeight()
+        };
+        const screenshot = Tools.CreateScreenshot(engine, camera as Camera, size);
+        console.log("screenshot created", screenshot);
+      }
     });
-    
-};
+  };
 
-const bjsCanvas = ref(null);
-useCanvatoriumScene(bjsCanvas, createLabContent);
+  const bjsCanvas = ref(null);
+  useCanvatoriumScene(bjsCanvas, createLabContent);
 </script>
 
 <template>
-    <canvas id="bjsCanvas" ref="bjsCanvas" ></canvas>
+  <canvas id="bjsCanvas" ref="bjsCanvas"></canvas>
 </template>
