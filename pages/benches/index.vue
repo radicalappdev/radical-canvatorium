38e172ccba186e07e5c441b0dfe0f371b45384ed
Joseph Simpson <joe@radicalappdev.com>
2023-09-21 07:11:15 -0400

    Adding a benches index

---
+++ pages/benches/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="max-w-screen-md h-[calc(100%_-_2rem)] overflow-y-auto container mx-auto">
+    <h2 class="text-lg font-bold my-4">All Benches</h2>
+    <nuxt-link v-for="route in labRoutes" :to="route.path" :key="route.path">
+      <p class="p-2 hover:bg-slate-100 drop-shadow-lg">{{ route.meta.title }}</p>
+    </nuxt-link>
+  </div>
+</template>
+
+<script setup>
+  const router = useRouter();
+
+  definePageMeta({
+    featured: false,
+    title: "All Benches",
+    description: "A list of all benches in the project. For now, these are just `nuxt-link` components, but I'll might convert this page to a Babylon JS scene."
+  });
+
+  const allRoutes = router.options.routes;
+
+  // Create a new array that only includes routes that begon with /labs and exlude the index page
+  const labRoutes = allRoutes.filter((route) => route.path.startsWith("/benches") && route.path !== "/benches");
+</script>
