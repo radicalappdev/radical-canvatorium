c7266f09b6f5025d4979da357baf1ff45c1d7630
Joseph Simpson <joe@radicalappdev.com>
2023-09-25 11:42:40 -0400

    Adding bench 006

---
+++ pages/benches/bench006.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+  import { Scene, Vector3, MeshBuilder, Mesh, StandardMaterial, Color3, Color4, ArcRotateCamera } from "babylonjs";
+  import projectData from "@/data/project-timeline.json";
+
+  definePageMeta({
+    featured: false,
+    title: "Bench 006 – 3D Layout Layers",
+    description: "Can I build a simple visual preview of FileMaker layouts in 3D?"
+  });
+
+  const createLabContent = async (scene: Scene) => {
+    scene.clearColor = Color4.FromHexString(labColors.slate6 + "ff");
+    const cam = scene.getCameraByName("camera") as ArcRotateCamera;
+  };
+
+  // If a lab uses the default options, you can just call useBabylonScene() with the bjsCanvas ref and the createLabContent function.
+  // Otherwise, you can pass in an options object with the following properties:
+  const labSceneOptions = {
+    useCamera: true,
+    useLights: true,
+    useRoom: false,
+    useOverlay: false,
+    useWebXRPlayer: false
+  };
+
+  const bjsCanvas = ref(null);
+  // With scene options
+  useCanvatoriumScene(bjsCanvas, createLabContent, labSceneOptions);
+
+  // Without scene options (see lab001 for an example)
+  // useCanvatoriumScene(bjsCanvas, createLabContent);
+</script>
+<template>
+  <canvas id="bjsCanvas" ref="bjsCanvas"></canvas>
+</template>
