08d2919a8c2ad4633764ac725bc7ca8c343a4d03
Joseph Simpson <joe@radicalappdev.com>
2024-04-18 07:50:56 -0400

    Adding some GUI to switch modes

--- pages/benches/bench005.vue
+++ pages/benches/bench005.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
-  import { Scene, ArcRotateCamera, Vector3 } from "@babylonjs/core";
+  import { Engine, Scene, ArcRotateCamera, Camera, Vector3, Color3, MeshBuilder, StandardMaterial } from "@babylonjs/core";
+  import { AdvancedDynamicTexture, StackPanel, Control, Button } from "@babylonjs/gui";
 
   definePageMeta({
     featured: false,
@@ -8,23 +9,93 @@
     labNotes: ``
   });
 
-  const createLabContent = async (scene: Scene) => {
-    // Implement the lab/bench here
+  const cameraOrthoSize = ref(10);
+  const cameraMode = ref("perspective");
 
-    const targetPos = new Vector3(0, 0, 0);
-    const camera = new ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 8, targetPos, scene);
+  const createLabContent = async (scene: Scene) => {
+    const engine = scene.getEngine();
+
+    const boxMaterial = new StandardMaterial("box-mat", scene);
+    boxMaterial.diffuseColor = Color3.FromHexString(labColors.purple);
+
+    const subject = MeshBuilder.CreateBox("box", { size: 1 }, scene);
+    subject.material = boxMaterial;
+
+    const camera = new ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 8, subject.position, scene);
     camera.wheelDeltaPercentage = 0.01;
     camera.upperBetaLimit = Math.PI / 1.5;
     camera.lowerRadiusLimit = 2;
     camera.upperRadiusLimit = 20;
     camera.attachControl(bjsCanvas, true);
+
+    // watch for changes in cameraMode
+    watch(cameraMode, (value) => {
+      if (value === "orthographic") {
+        camera.mode = Camera.ORTHOGRAPHIC_CAMERA;
+        adjustOrthoSize(camera, engine, cameraOrthoSize.value);
+      } else {
+        camera.mode = Camera.PERSPECTIVE_CAMERA;
+      }
+    });
+
+    createOverlay(scene);
+  };
+
+  // Calculate the ortho size based on current engine size
+  const adjustOrthoSize = (camera: Camera, engine: Engine, value: number) => {
+    camera.orthoTop = engine.getRenderHeight() / value / 10;
+    camera.orthoBottom = -(engine.getRenderHeight() / value / 10);
+    camera.orthoLeft = -(engine.getRenderWidth() / value / 10);
+    camera.orthoRight = engine.getRenderWidth() / value / 10;
+  };
+
+  const createOverlay = (scene: Scene) => {
+    const advancedTexture = AdvancedDynamicTexture.CreateFullscreenUI("lab-overlay", true, scene);
+
+    const button1 = Button.CreateSimpleButton("button1", "Ortho Mode");
+    button1.top = window.innerHeight / 2 - 60 + "px";
+    button1.left = "100px";
+    button1.width = "200px";
+    button1.height = "40px";
+    button1.color = "white";
+    button1.background = labColors.slate6;
+    button1.onPointerClickObservable.add(() => {
+      console.log("button1 clicked");
+      cameraMode.value = "orthographic";
+    });
+    advancedTexture.addControl(button1);
+
+    const button2 = Button.CreateSimpleButton("button2", "Perspective Mode");
+    button2.top = window.innerHeight / 2 - 60 + "px";
+    button2.left = "-100px";
+    button2.width = "200px";
+    button2.height = "40px";
+    button2.color = "white";
+    button2.background = labColors.purple;
+    button2.onPointerClickObservable.add(() => {
+      console.log("button2 clicked");
+      cameraMode.value = "perspective";
+    });
+
+    advancedTexture.addControl(button2);
+
+    watch(cameraMode, (value) => {
+      if (value === "orthographic") {
+        button1.background = labColors.purple;
+        button2.background = labColors.slate6;
+      } else {
+        button1.background = labColors.slate6;
+        button2.background = labColors.purple;
+      }
+    });
   };
 
   const labSceneOptions = {
     useCamera: false,
     useLight: true,
-    useRoom: true,
-    useWebXRPlayer: false
+    useRoom: false,
+    useWebXRPlayer: false,
+    useOverlay: false
   };
 
   const bjsCanvas = ref(null);
